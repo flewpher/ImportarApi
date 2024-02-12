d6fdcd36145d0b0200149fc4fddc550489142e4c
flewpher <danielalonsomoreno5606@gmail.com>
2024-02-12 10:55:45 +0100

    renamed:    json/data_prueba.json -> data_prueba.json 	modified:   index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include(__DIR__."/controller/controlador.php");
 
-$json=__DIR__."/json/data_prueba.json";
+$json=__DIR__."/data_prueba.json";
 
 publicar_productos($json);
 //print_r(Conexion::getConexion()->get("products"));
