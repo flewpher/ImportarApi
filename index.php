fd263623160cdcc5993b077408832d573e2d42c5
flewpher <danielalonsomoreno5606@gmail.com>
2024-02-12 10:46:45 +0100

    modified:   index.php

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ include(__DIR__."/controller/controlador.php");
 $json=__DIR__."/json/data_prueba.json";
 
 publicar_productos($json);
-print_r(Conexion::getConexion()->get("products"));
+//print_r(Conexion::getConexion()->get("products"));
